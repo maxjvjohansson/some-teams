002fe896fc1235151443580f52ba501079a1a7aa
maxjvjohansson <max.jv.johansson@gmail.com>
2024-10-01 15:48:31 +0200

    Added basic HTML structure to header.php and footer.php

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Some Teams</title>
+
+    <link rel="stylesheet" href="/css/styles.css">
+
+</head>
+
+<body>
+
+    <header>
+        <h1>Welcome to the Football Teams Website</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+        </nav>
+    </header>
