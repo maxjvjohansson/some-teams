e009103ed472b70315f5e5d4e8486a737fe756a0
maxjvjohansson <max.jv.johansson@gmail.com>
2024-10-01 16:37:20 +0200

    set up foreach loop in teams.php

--- includes/teams.php
+++ includes/teams.php
@@ -1,3 +1,16 @@
 <?php
 
 require_once __DIR__ . "/../data.php";
+
+?>
+
+<section class="team-list">
+    <?php foreach ($teams as $teamName => $teamData) { ?>
+        <div class="team-card">
+            <a href="<?= $teamData["url"]; ?>" target="_blank">
+                <img src="<?= $teamData["logo"]; ?>" alt="<?= $teamName; ?>Logo"></a>
+            <h2><?= $teamName; ?> </h2>
+            <p> UEFA Ranking: <?= $teamData["uefa-coefficient-ranking"]; ?></p>
+        </div>
+    <?php } ?>
+</section>
