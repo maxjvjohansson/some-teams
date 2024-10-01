a5fbdf0d5872a68ba5b9ad966fd92930aaed76a6
maxjvjohansson <max.jv.johansson@gmail.com>
2024-10-01 16:13:36 +0200

    Linked php files using require

---
+++ about.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once __DIR__ . "/includes/header.php";
+require_once __DIR__ . "/includes/footer.php";
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . "/includes/header.php";
+require_once __DIR__ . "/includes/teams.php";
+require_once __DIR__ . "/includes/footer.php";
