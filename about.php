8d0db3f29b1560ace00e9eb2c9097376c503a139
maxjvjohansson <max.jv.johansson@gmail.com>
2024-10-02 18:17:31 +0200

    added favicon, added text to about page

--- about.php
+++ about.php
@@ -1,4 +1,27 @@
 <?php
 
 require_once __DIR__ . "/includes/header.php";
+
+?>
+
+<h2>About the Creator</h2>
+<p>
+    Hello! My name is Max, and I am a student at Yrgo studying web development.
+    I have a passion for web development, and this project is a demonstration
+    of my skills in PHP, HTML, and CSS.</p>
+
+<h2>About the Project</h2>
+<p>
+    This website was built as part of a school project to demonstrate my
+    ability to create dynamic web pages using PHP.</p>
+
+<h2>Technologies Used</h2>
+<ul>
+    <li>PHP for server-side logic and handling the data.</li>
+    <li>HTML and CSS for structuring and styling the web page.</li>
+    <li>GitHub for version control and managing code commits.</li>
+</ul>
+
+<?php
+
 require_once __DIR__ . "/includes/footer.php";
--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Some Teams</title>
 
+    <link rel="icon" type="image/png" href="/images/football_vector.png">
     <link rel="stylesheet" href="/css/styles.css">
 
 </head>
